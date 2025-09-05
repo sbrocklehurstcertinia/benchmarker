1c3fc486e2227985793f8f75d2af5d6d9449bc0b
Shaun Brocklehurst <sbrocklehurst@certinia.com>
2025-09-05 15:04:31 +0100

    Add new table

--- db/init.sql
+++ db/init.sql
@@ -66,6 +66,7 @@ CREATE TABLE IF NOT EXISTS performance.test_result (
     source_ref text
 );
 
+
 CREATE TABLE IF NOT EXISTS performance.alert (
     id serial PRIMARY KEY,
     create_date_time timestamp without time zone DEFAULT now() NOT NULL,
@@ -82,6 +83,59 @@ CREATE TABLE IF NOT EXISTS performance.alert (
 );
 
 CREATE TABLE IF NOT EXISTS performance.test_info (
+    id serial PRIMARY KEY,
+    create_date_time timestamp without time zone DEFAULT now() NOT NULL,
+    update_date_time timestamp without time zone DEFAULT now() NOT NULL,
+    flow_name text,
+    action text,
+    product text,
+    additional_data text
+);
+
+CREATE TABLE IF NOT EXISTS performance.ui_test_result (
+    id serial PRIMARY KEY,
+    create_date_time timestamp without time zone DEFAULT now() NOT NULL,
+    update_date_time timestamp without time zone DEFAULT now() NOT NULL,
+    duration integer,
+    action text,
+    flow_name text,
+    error text,
+    target_value integer,
+    product text,
+    incognito_browser boolean,
+    speed_index integer,
+    time_to_interactive integer,
+    dlp_lines integer,
+    dp_documents integer,
+    test_type text,
+    cpu_time integer,
+    dml_rows integer,
+    dml_statements integer,
+    heap_size integer,
+    query_rows integer,
+    soql_queries integer,
+    queueable_jobs integer,
+    future_calls integer,
+    source_ref text
+);
+
+
+CREATE TABLE IF NOT EXISTS performance.ui_alert (
+    id serial PRIMARY KEY,
+    create_date_time timestamp without time zone DEFAULT now() NOT NULL,
+    update_date_time timestamp without time zone DEFAULT now() NOT NULL,
+    test_result_id integer,
+    flow_name text,
+    action text,
+    cpu_time_degraded integer,
+    dml_rows_degraded integer,
+    dml_statements_degraded integer,
+    heap_size_degraded integer,
+    query_rows_degraded integer,
+    soql_queries_degraded integer
+);
+
+CREATE TABLE IF NOT EXISTS performance.ui_test_info (
     id serial PRIMARY KEY,
     create_date_time timestamp without time zone DEFAULT now() NOT NULL,
     update_date_time timestamp without time zone DEFAULT now() NOT NULL,
